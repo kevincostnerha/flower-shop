e2310c230a7d6764b34a41c06eb5b3795571913a
Kevin Ha <kevincostnerha@gmail.com>
2021-11-19 15:35:50 -0800

    made some more lwc and made adjustement to old ones

--- force-app/main/default/lwc/fieldImage/fieldImage.js
+++ force-app/main/default/lwc/fieldImage/fieldImage.js
@@ -3,7 +3,6 @@ import My_Resource from '@salesforce/resourceUrl/myResource';
 
 
 export default class HelloWorld extends LightningElement {
-  red = My_Resource + '/red00.jpeg';
-  white = My_Resource + '/white00.jpeg';
-  pink = My_Resource + '/pink00.jpeg';
+  field = My_Resource + '/field.jpg';
+
 }
